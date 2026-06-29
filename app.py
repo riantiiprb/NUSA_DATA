07c81f8b2a16e1746f5b0362041e4e3244b6ea95
riantiiprb <Riantisrilestaripurba@mail.ugm.ac.id>
2026-06-29 13:09:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,58 @@ st.set_page_config(
 page_title="NUSA DATA",
 layout="wide"
 )
+st.markdown("""
+<style>
+
+.stApp{
+    background:
+    linear-gradient(
+    135deg,
+    #eef6ff,
+    #ffffff
+    );
+}
 
 
-st.title(
-"NUSA DATA"
+.main-title{
+    font-size:55px;
+    font-weight:800;
+    color:#0f172a;
+}
+
+
+.subtitle{
+    font-size:22px;
+    color:#475569;
+}
+
+
+.card{
+    background:white;
+    padding:25px;
+    border-radius:20px;
+    box-shadow:0px 8px 25px rgba(0,0,0,0.08);
+    height:180px;
+}
+
+
+</style>
+""",
+unsafe_allow_html=True)
+
+
+st.markdown(
+"""
+<div class="main-title">
+NUSA DATA
+</div>
+
+<div class="subtitle">
+National Unified Statistics and Development Analytics
+</div>
+
+""",
+unsafe_allow_html=True
 )
 
 
