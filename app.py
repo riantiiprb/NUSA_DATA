aee8859e698b3e678389bb33f0b9086b33691e8e
riantiiprb <Riantisrilestaripurba@mail.ugm.ac.id>
2026-06-30 10:32:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 import joblib
 import os
+import pandas as pd
+
 
 st.set_page_config(
-page_title="NUSA DATA",
-layout="wide"
+    page_title="NUSA DATA",
+    layout="wide"
 )
+
 
 # =====================
 # CSS DESIGN
@@ -44,14 +47,63 @@ st.markdown("""
     border-radius:10px;
 }
 
-
 </style>
 """,
 unsafe_allow_html=True)
 
+
+
+# =====================
+# UPLOAD GLOBAL
+# =====================
+
+st.sidebar.header("Dataset")
+
+
+uploaded = st.sidebar.file_uploader(
+    "Upload Dataset",
+    type=["csv","xlsx"]
+)
+
+
+if uploaded:
+
+    if uploaded.name.endswith(".xlsx"):
+        df = pd.read_excel(uploaded)
+    else:
+        df = pd.read_csv(uploaded)
+
+
+    st.session_state["data"] = df
+
+    st.sidebar.success(
+        "Dataset tersimpan ✓"
+    )
+
+
+elif "data" in st.session_state:
+
+    st.sidebar.success(
+        "Dataset aktif ✓"
+    )
+
+
+else:
+
+    st.sidebar.info(
+        "Upload dataset dulu"
+    )
+
+
+
+# =====================
+# HEADER
+# =====================
+
 st.subheader(
 "National Unified Statistics and Development Analytics"
 )
+
 
 
 a,b,c = st.columns(3)
