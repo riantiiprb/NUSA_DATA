2f1fc52a51c00d10b2539c800a2a3644535cc126
riantiiprb <Riantisrilestaripurba@mail.ugm.ac.id>
2026-06-30 11:08:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,62 @@
+import streamlit as st
+import pandas as pd
+import joblib
+import os
+
+
+st.set_page_config(
+    page_title="NUSA DATA",
+    layout="wide"
+)
+
+
+# =====================
+# CSS DESIGN
+# =====================
+
+st.markdown("""
+<style>
+
+/* sidebar background */
+[data-testid="stSidebar"]{
+    background:
+    linear-gradient(
+    180deg,
+    #0f172a,
+    #1e3a8a
+    );
+}
+
+
+[data-testid="stSidebar"] *{
+    color:white;
+}
+
+
+[data-testid="stSidebarNavLink"]:hover{
+    background-color:#2563eb;
+    border-radius:10px;
+}
+
+
+[data-testid="stSidebarNavLink"][aria-current="page"]{
+    background-color:#3b82f6;
+    border-radius:10px;
+}
+
+
+</style>
+""",
+unsafe_allow_html=True)
+
+
+
+st.subheader(
+"National Unified Statistics and Development Analytics"
+)
+
+
+
 # =====================
 # DATA UPLOAD
 # =====================
@@ -24,6 +83,7 @@ if pembangunan:
 
     if pembangunan.name.endswith(".xlsx"):
         df_pembangunan = pd.read_excel(pembangunan)
+
     else:
         df_pembangunan = pd.read_csv(pembangunan)
 
@@ -41,6 +101,7 @@ if aspirasi:
 
     if aspirasi.name.endswith(".xlsx"):
         df_aspirasi = pd.read_excel(aspirasi)
+
     else:
         df_aspirasi = pd.read_csv(aspirasi)
 
@@ -50,4 +111,30 @@ if aspirasi:
 
     st.success(
         "Dataset aspirasi tersimpan"
+    )
+
+
+
+# =====================
+# CARD KAMU TETAP
+# =====================
+
+a,b,c = st.columns(3)
+
+
+with a:
+    st.info(
+    "Development Priority Engine\n\nClustering wilayah"
+    )
+
+
+with b:
+    st.info(
+    "Citizen Voice Intelligence\n\nNLP aspirasi"
+    )
+
+
+with c:
+    st.info(
+    "NUSA MATCH\n\nAI Recommendation"
     )
