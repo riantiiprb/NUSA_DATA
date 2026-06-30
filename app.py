a8071f99a43e3dc3b94decac0d1a20ae201a0430
riantiiprb <Riantisrilestaripurba@mail.ugm.ac.id>
2026-06-30 11:03:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,127 +1,53 @@
-import streamlit as st
-import joblib
-import os
-import pandas as pd
+# =====================
+# DATA UPLOAD
+# =====================
+
+st.header("Upload Dataset")
 
 
-st.set_page_config(
-    page_title="NUSA DATA",
-    layout="wide"
+pembangunan = st.file_uploader(
+    "Dataset Pembangunan (BPS)",
+    type=["csv","xlsx"],
+    key="upload_pembangunan"
 )
 
 
-# =====================
-# CSS DESIGN
-# =====================
-
-st.markdown("""
-<style>
-
-/* sidebar background */
-[data-testid="stSidebar"]{
-    background:
-    linear-gradient(
-    180deg,
-    #0f172a,
-    #1e3a8a
-    );
-}
-
-
-/* tulisan menu sidebar */
-[data-testid="stSidebar"] *{
-    color:white;
-}
-
-
-/* hover menu */
-[data-testid="stSidebarNavLink"]:hover{
-    background-color:#2563eb;
-    border-radius:10px;
-}
-
-
-/* menu aktif */
-[data-testid="stSidebarNavLink"][aria-current="page"]{
-    background-color:#3b82f6;
-    border-radius:10px;
-}
-
-</style>
-""",
-unsafe_allow_html=True)
-
-
-
-# =====================
-# UPLOAD GLOBAL
-# =====================
-
-st.sidebar.header("Dataset")
-
-
-uploaded = st.sidebar.file_uploader(
-    "Upload Dataset",
-    type=["csv","xlsx"]
+aspirasi = st.file_uploader(
+    "Dataset Aspirasi Masyarakat",
+    type=["csv","xlsx"],
+    key="upload_aspirasi"
 )
 
 
-if uploaded:
 
-    if uploaded.name.endswith(".xlsx"):
-        df = pd.read_excel(uploaded)
+if pembangunan:
+
+    if pembangunan.name.endswith(".xlsx"):
+        df_pembangunan = pd.read_excel(pembangunan)
     else:
-        df = pd.read_csv(uploaded)
+        df_pembangunan = pd.read_csv(pembangunan)
 
 
-    st.session_state["data"] = df
-
-    st.sidebar.success(
-        "Dataset tersimpan ✓"
-    )
+    st.session_state["pembangunan"] = df_pembangunan
 
 
-elif "data" in st.session_state:
-
-    st.sidebar.success(
-        "Dataset aktif ✓"
-    )
-
-
-else:
-
-    st.sidebar.info(
-        "Upload dataset dulu"
+    st.success(
+        "Dataset pembangunan tersimpan"
     )
 
 
 
-# =====================
-# HEADER
-# =====================
+if aspirasi:
 
-st.subheader(
-"National Unified Statistics and Development Analytics"
-)
-
+    if aspirasi.name.endswith(".xlsx"):
+        df_aspirasi = pd.read_excel(aspirasi)
+    else:
+        df_aspirasi = pd.read_csv(aspirasi)
 
 
-a,b,c = st.columns(3)
+    st.session_state["aspirasi"] = df_aspirasi
 
 
-with a:
-    st.info(
-    "Development Priority Engine\n\nClustering wilayah"
-    )
-
-
-with b:
-    st.info(
-    "Citizen Voice Intelligence\n\nNLP aspirasi"
-    )
-
-
-with c:
-    st.info(
-    "NUSA MATCH\n\nAI Recommendation"
+    st.success(
+        "Dataset aspirasi tersimpan"
     )
